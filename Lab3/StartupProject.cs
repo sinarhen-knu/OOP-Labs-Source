2de2a15ceff6a3dd4cf9f53a800fb285cddb0a71
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-04-08 12:51:43 +0300

    upd lab 3

---
+++ Lab3/StartupProject.cs
@@ -0,0 +1,77 @@
+﻿using Utils;
+
+namespace Lab3;
+using Lab2;
+
+public static class StartupProject
+{
+    // 3 Functions from lab 2
+    private static void FindCharactersInParentheses()
+    {
+        var input = Input.ReadAndValidateInput("Enter a string: ", s => s, s => s.Length > 0);
+
+        var stack = new Stack<char>();
+        var result = "";
+
+        foreach (var c in input)
+            switch (c)
+            {
+                case '(':
+                    stack.Push(c);
+                    break;
+                case ')':
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                        result += " ";
+                    }
+
+                    break;
+                }
+                default:
+                {
+                    if (stack.Count > 0) result += c;
+
+                    break;
+                }
+            }
+
+        Console.WriteLine($"Characters within parentheses: {result}");
+    }
+
+    private static void SolveNonLinearEquation()
+    {
+        double a = -10;
+        double b = 10;
+        var epsilon = 0.0001;
+
+        if (Function(a) * Function(b) >= 0)
+        {
+            Console.WriteLine("You have not assumed right a and b\n");
+            return;
+        }
+
+        var c = a;
+
+        while (b - a >= epsilon)
+        {
+            c = (a + b) / 2;
+
+            if (Function(c) == 0.0)
+                break;
+
+            if (Function(c) * Function(a) < 0)
+                b = c;
+            else
+                a = c;
+        }
+
+        Console.WriteLine($"The value of root is : {c}");
+    }
+
+    private static double Function(double x)
+    {
+        return 5 * Math.Pow(x, 4) + 4 * Math.Pow(x, 3) - 6 * Math.Pow(x, 2) + 5;
+    }
+}
---
+++ Lab3/UniversityCalc.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab3;
+
+public partial class University
+{
+    public double CalculateAnnualFunding()
+    {
+        // Switch case for Address
+        double locationFactor = City switch
+        {
+            City.LosAngeles => 1.2,
+            City.Kyiv => 1.1,
+            City.SanFrancisco => 1.3,
+            City.NewYork => 1.4,
+            City.London => 1.5,
+            City.Paris => 1.6,
+            City.Tokyo => 1.7,
+            City.Sydney => 1.8,
+            City.Berlin => 1.9,
+            City.Madrid => 2.0,
+            _ => 1.0
+        };
+
+        // Switch case for overallStudentsCount
+        var sizeFactor = overallStudentsCount switch
+        {
+            < 1000 => 0.8,
+            < 5000 and >= 1000 => 1.0,
+            >= 5000 => 1.2,
+        };
+
+        // Switch case for rating
+        var ratingFactor = rating switch
+        {
+            < 100 => 0.5,
+            < 200 and >= 100 => 0.7,
+            < 300 and >= 200 => 1,
+            < 400 and >= 300 => 1.2,
+            < 500 and >= 400 => 1.5,
+            < 600 and >= 500 => 2,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        
+        var fundingPerStudent = (rating / 100.0) * (2000 + 4000) + 2000;
+        var annualFunding = fundingPerStudent * overallStudentsCount * sizeFactor * locationFactor * ratingFactor;
+
+        return annualFunding;
+    }
+}
