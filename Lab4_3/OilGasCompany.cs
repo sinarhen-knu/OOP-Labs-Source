4483f48bc2e5324b36779cb5e627693cada35ba8
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-05-20 12:27:51 +0300

    separated classes

---
+++ Lab4_3/OilGasCompany.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab4;
+
+public class OilGasCompany : Enterprise
+{
+    private double productionVolume;
+
+    public OilGasCompany(string name, string location, string activityField, double successRating, double productionVolume)
+        : base(name, location, activityField, successRating)
+    {
+        this.productionVolume = productionVolume;
+    }
+
+    public double ProductionVolume
+    {
+        get => productionVolume;
+        set => productionVolume = value;
+    }
+
+    public override double CalculateProfit(double productValuePercentage)
+    {
+        return productionVolume * productValuePercentage;
+    }
+
+    public override int CalculateEmployeeCount(double salaryFund, double averageSalary)
+    {
+        return (int)(salaryFund / averageSalary);
+    }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine($"Production Volume: {productionVolume}");
+    }
+}
